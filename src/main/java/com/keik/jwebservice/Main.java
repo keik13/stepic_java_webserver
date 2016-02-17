6b6c1d3471c4f102f9098bd4349e8f19275c7183
a.koba <a.koba@bftcom.com>
2016-02-17 17:59:59 +0300

    first commit

---
+++ src/main/java/com/keik/jwebservice/Main.java
@@ -0,0 +1,29 @@
+package com.keik.jwebservice;
+
+import com.keik.jwebservice.servlets.AllRequestsServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * @author Коба Андрей Александрович.
+ */
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+    context.addServlet(new ServletHolder(allRequestsServlet), "/mirror");
+
+    Server server = new Server(8080);
+    server.setHandler(context);
+
+    server.start();
+    System.out.println("Server started");
+    server.join();
+
+  }
+
+}
---
+++ src/main/java/com/keik/jwebservice/servlets/Frontend.java
@@ -0,0 +1,20 @@
+package com.keik.jwebservice.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Коба Андрей Александрович.
+ */
+public class Frontend extends HttpServlet{
+  private String login = "";
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+  }
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+  }
+}
